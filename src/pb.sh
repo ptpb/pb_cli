e6fd3ec0baa2a953f2a931a5c8689d245b30edf6
Oliver Albertini <oliver.ruben@gmail.com>
2018-07-24 18:04:37 -0700

    Implement clipboard functionality
    * clipboard tool is dictated as `xclip` by default
    * tool can be overridden with PB_CLIPBOARD_TOOL
    * use of clipboard only occurs if PB_CLIPBOARD is not empty

--- src/pb.sh
+++ src/pb.sh
@@ -3,7 +3,8 @@
 endpoint="${PB_ENDPOINT:-https://ptpb.pw}"
 jq_args="${PB_JSON:--r .url}"
 private="${PB_PRIVATE:-0}"
-
+clipboard="${PB_CLIPBOARD}"
+clipboard_tool="${PB_CLIPBOARD_TOOL:-xclip}"
 
 pb_ () {
   local filename extension
@@ -13,36 +14,25 @@ pb_ () {
 
   shift 2
 
-  curl -sF "c=@$filename" -F "f=-$extension" -F "p=$private" \
-       -H 'accept: application/json' "$@" "$endpoint" | jq $jq_args
+  data=$(curl -sF "c=@$filename" -F "f=-$extension" -F "p=$private" \
+           -H 'accept: application/json' "$@" "$endpoint" | jq $jq_args)
+  if [[ ! -z $clipboard ]]; then
+    printf "${data}" | "${clipboard_tool}"
+  fi
+  echo "${data}"
 }
-
 
 pb_png () {
   maim -s | pb_ - .png
 }
 
-
 pb_gif () {
   capture gif - | pb_ - .gif
 }
 
-
 pb_webm () {
   capture webm - | pb_ - .webm
 }
-
-pb_clipboard () {
-  if [[ $(uname) == [Ll]inux ]]; then
-    pb_ "$@" | xclip
-  elif [[ $(uname) == Darwin ]]; then
-    pb_ "$@" | pbcopy
-  else
-    echo "Your system clipboard is unknown."
-    return
-  fi
-}
-
 
 pb () {
   local command="$1"
@@ -64,10 +54,6 @@ pb () {
       shift
       private=1
       pb_ "$@"
-      ;;
-    clipboard)
-      shift
-      pb_clipboard "$@"
       ;;
     *)
       pb_ "$@"
